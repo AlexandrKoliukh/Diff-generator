c1f620fcd3251ceef7676eef75d15718e42f06e5
AlexandrKoliukh <alexkoliukh@gmail.com>
2019-09-12 18:12:51 +0300

    chore

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,5 +5,8 @@ export default () => {
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'Output format')
+    .action(() => {
+      console.log(commander.on('--help'))
+    })
     .parse(process.argv);
 };
