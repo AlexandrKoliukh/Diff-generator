7e62f70805d83c727b857443a8c6cf36696e749f
AlexandrKoliukh <alexkoliukh@gmial.com>
2019-10-01 08:12:18 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ export default () => {
   commander
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .arguments('<firstConfig> <secondConfig>')
     .option('-f, --format [type]', 'Output format')
+    .action((firstConfig, secondConfig) => {
+      console.log(firstConfig);
+      console.log(secondConfig);
+    })
     .parse(process.argv);
 };
