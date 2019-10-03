7c656f125a1b132aa510e5090e78680e840c3aec
AlexandrKoliukh <alexkoliukh@gmial.com>
2019-10-03 18:50:21 +0300

    tree array

--- src/formatters/plain/plainView.js
+++ src/formatters/plain/plainView.js
@@ -1,12 +1,10 @@
-import _ from 'lodash';
-
-const getKeys = (obj1, obj2) => _.union(_.keys(obj1), _.keys(obj2));
+import { isNaN, flattenDeep } from 'lodash';
 
 const types = {
   object: () => '[complex value]',
   number: (value) => value,
   boolean: (value) => value,
-  string: (value) => (_.isNaN(+value) ? `'${value}'` : value),
+  string: (value) => (isNaN(+value) ? `'${value}'` : value),
 };
 
 const getTypeValue = (value) => types[typeof value](value);
@@ -14,16 +12,20 @@ const getTypeValue = (value) => types[typeof value](value);
 const getStartString = (property, name) => `Property '${property}${name}' was`;
 
 const plainView = (data) => {
-  const iter = (diff, property = '') => getKeys(diff).reduce((acc, key) => {
-    const startString = getStartString(property, key);
+  const iter = (diff, property = '') => diff.map((item) => {
+    const {
+      name, oldValue, newValue, state, children,
+    } = item;
+    const startString = getStartString(property, name);
 
-    switch (diff[key].state) {
-      case 'parent': return [...acc, iter(diff[key].children, `${property}${key}.`)];
-      case 'unchanged': return '';
-      case 'added': return [...acc, `${startString} added with value: ${getTypeValue(diff[key].newValue)}`];
-      case 'deleted': return [...acc, `${startString} removed`];
-      case 'changed': return [...acc,
-        `${startString} updated. From ${getTypeValue(diff[key].oldValue)} to ${getTypeValue(diff[key].newValue)}`];
+    switch (state) {
+      case 'parent': return iter(children, `${property}${name}.`);
+      case 'unchanged': return null;
+      case 'added': return `${startString} added with value: ${getTypeValue(newValue)}`;
+      case 'deleted': return `${startString} removed`;
+      case 'changed': {
+        return `${startString} updated. From ${getTypeValue(oldValue)} to ${getTypeValue(newValue)}`;
+      }
       default: return new Error('Invalid state');
     }
   }, []);
@@ -31,4 +33,6 @@ const plainView = (data) => {
   return iter(data);
 };
 
-export default (data) => _.flattenDeep(plainView(data)).join('\n');
+export default (data) => flattenDeep(plainView(data))
+  .filter((i) => i)
+  .join('\n');
